ed5fc3545a4d03f2f0e09df1afb12cdc24343261
Jeong Hyun Kim <jhkim0508@gmail.com>
2014-08-04 22:59:09 -0500

    Review words functions

--- day1/words.py
+++ day1/words.py
@@ -1,5 +1,32 @@
-file = open('words.txt')
+#file = open('words.txt')
 
-for line in file:
-  word = line.strip()
-  print word
+#for line in file:
+ # word = line.strip()
+  #print word
+  
+def has_no_e(word):
+ return not "e" in word
+
+print has_no_e("hi")
+print has_no_e("hello")
+
+
+def uses_only(word, available):
+ for letter in word:
+  if not letter in available: return False
+ return True
+
+print uses_only("abbcc", "abc")
+print uses_only("abbccd", "abc")
+
+def uses_all(word, available):
+ return uses_only(available, word)
+ 
+print uses_all("ab", "abc")
+print uses_all("abcc", "abc")
+
+def is_abecedarian(word):
+ return "".join(sorted(word))==word # join the string elements of the list word, separated by ""
+ 
+print is_abecedarian("randomword")
+print is_abecedarian("abcde")
