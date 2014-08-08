2a45a67824c4ad7a493f63d116c580c60384e7ad
Jeong Hyun Kim <jhkim0508@gmail.com>
2014-08-07 22:26:51 -0500

    HW 1 updates: add and grade

--- day2/school.py
+++ day2/school.py
@@ -2,31 +2,26 @@ class School:
  def __init__(self, schoolname):
   self.schoolname = schoolname
   self.db = {}
-  self.students = []
-  self.grades = []
   
  def add(self, student, grade):
-  self.student = student
-  self.grade = grade
-  self.students.append(self.student)
-  self.grades.append(self.grade)
-  #self.db = dict(zip(self.grades, self.students))
-  self.db.setdefault(self.grade, {})
- # self.db[self.grade].append(self.students)
-  self.db.update({self.grade: self.student})
- 
+  if grade in self.db:
+   self.db[grade].add(student)
+  else:
+   self.db[grade] = {student}
+
 
  def grade(self, key): # method to call values corresponding to key
-  self.key = key
-  return self.db.get(self.key, "None")
+  return self.db.get(key, "None") # It will return the default value "None", if key is not available. Otherwise, it returns a value for a given key
   
 school = School("Haleakala Hippy School")
 school.add("James", 2)
 school.add("Blair", 2)
-school.add("Paul", 3)
+school.add("Paul", 2)
 
 print school.db
-print school.db[2]
+print school.db
+print school.grade(4)
+
 #print school.db[2]
 #print school.db.values()
 #print school.db.get(2, "none")
